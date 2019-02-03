0e5b0b58bbc7460cf2580d260b685c4a8892280f
Denis K <f1nal@cgaming.org>
2019-02-03 22:06:09 +0300

    Add sdk example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { getPublicApiService } from 'jet-admin-sdk';
+
 import './App.css';
 
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  getPublicApiService().projectsStore.setCurrent('YOUR_PROJECT_UNIQUE_NAME');
+  getPublicApiService().authService.tokenLogin('YOUR_USER_TOKEN');
+}
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { user: 'unknown' };
+  }
+
+  componentDidMount() {
+    const self = this;
+    getPublicApiService().currentUserStore.get().subscribe(result => {
+      self.setState({ user: result ? result.username : 'unknown' });
+    });
+  }
+
   render() {
     return (
     <p className="custom-view-element">
-      jet-test-react works!
+      jet-test-react works!<br/>
+      current user: {this.state.user}
     </p>
     );
   }
